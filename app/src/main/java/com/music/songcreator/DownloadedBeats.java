157302f309c19615511ba460d5a7f83e83dc1fe6
jtclapp <67022934+jtclapp@users.noreply.github.com>
2020-09-08 21:39:10 -0500

    Added in Final Changes for 1.0.1

--- app/src/main/java/com/music/songcreator/DownloadedBeats.java
+++ app/src/main/java/com/music/songcreator/DownloadedBeats.java
@@ -11,14 +11,12 @@ import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -26,7 +24,6 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FileDownloadTask;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-
 import java.io.File;
 import java.io.IOException;
 
@@ -34,12 +31,11 @@ public class DownloadedBeats extends AppCompatActivity {
     FirebaseStorage firebaseStorage;
     StorageReference storageReference;
     StorageReference ref;
-    RadioGroup radioGroup;
-    RadioButton RB1,RB2,RB3,RB4,RB5,RB6,RB7,RB8,RB9,RB10,RB11,RB12,RB13,RB14,RB15;
-    RadioButton RB16,RB17,RB18,RB19,RB20,RB21,RB22,RB23;
     ImageButton B1;
     String child;
     String path;
+    ArrayAdapter<String> adapter;
+    Spinner BeatSpinner;
     boolean connected;
     private AdView mAdView;
     @Override
@@ -51,232 +47,42 @@ public class DownloadedBeats extends AppCompatActivity {
         mAdView.loadAd(adRequest);
 
         B1 = findViewById(R.id.DownloadButton);
-        radioGroup = findViewById(R.id.RadioGroup_downloads);
-        RB1 = findViewById(R.id.radioButton1);
-        RB2 = findViewById(R.id.radioButton2);
-        RB3 = findViewById(R.id.radioButton3);
-        RB4 = findViewById(R.id.radioButton4);
-        RB5 = findViewById(R.id.radioButton5);
-        RB6 = findViewById(R.id.radioButton6);
-        RB7 = findViewById(R.id.radioButton7);
-        RB8 = findViewById(R.id.radioButton8);
-        RB9 = findViewById(R.id.radioButton9);
-        RB10 = findViewById(R.id.radioButton24);
-        RB11 = findViewById(R.id.radioButton25);
-        RB12 = findViewById(R.id.radioButton35);
-        RB13 = findViewById(R.id.radioButton26);
-        RB14 = findViewById(R.id.radioButton27);
-        RB15 = findViewById(R.id.radioButton28);
-        RB16 = findViewById(R.id.radioButton29);
-        RB17 = findViewById(R.id.radioButton10);
-        RB18 = findViewById(R.id.radioButton11);
-        RB19 = findViewById(R.id.radioButton12);
-        RB20 = findViewById(R.id.radioButton13);
-        RB21 = findViewById(R.id.radioButton126);
-        RB22 = findViewById(R.id.radioButton127);
-        RB23 = findViewById(R.id.radioButton36);
-        RB1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                child = "Rap Beat 1.mp3";
-                path = "HipHop_Beat#1.mp3";
-                B1.setActivated(true);
+        BeatSpinner = findViewById(R.id.Beat_spinner);
 
-            }
-        });
-        RB2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                child = "Rap Beat 2.mp3";
-                path = "HipHop_Beat#2.mp3";
-                B1.setActivated(true);
+        String[] arrayOfStrings = new String[23];
+        arrayOfStrings[0] = " Call Me Now";
+        arrayOfStrings[1] = " Blame Me";
+        arrayOfStrings[2] = " Uppercut";
+        arrayOfStrings[3] = " Only At Night";
+        arrayOfStrings[4] = " Too Hot For You";
+        arrayOfStrings[5] = " Just Go";
+        arrayOfStrings[6] = " Let Me In";
+        arrayOfStrings[7] = " Where Did You Go";
+        arrayOfStrings[8] = " Just By Myself";
+        arrayOfStrings[9] = " Champion Of The Fight";
+        arrayOfStrings[10] = " Unbeatable";
+        arrayOfStrings[11] = " Dad And I";
+        arrayOfStrings[12] = " Late Night";
+        arrayOfStrings[13] = " Scary Night";
+        arrayOfStrings[14] = " On My Grind";
+        arrayOfStrings[15] = " On My Mind";
+        arrayOfStrings[16] = " Out Like A Light";
+        arrayOfStrings[17] = " No Sleep For The Damned";
+        arrayOfStrings[18] = " She's With Someone Else";
+        arrayOfStrings[19] = " Party Time";
+        arrayOfStrings[20] = " Her Loss";
+        arrayOfStrings[21] = " This Is My Town";
+        arrayOfStrings[22] = " What I Love The Most";
 
-            }
-        });
-        RB3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                child = "Rap_Beat_3.mp3";
-                path = "HipHop_Beat#3.mp3";
-                B1.setActivated(true);
-            }
-        });
-        RB4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                child = "Rock Beat 1.mp3";
-                path = "Rock_Beat#1.mp3";
-                B1.setActivated(true);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrayOfStrings);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        BeatSpinner.setAdapter(adapter);
 
-            }
-        });
-        RB13.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                child = "Rock Beat 2.mp3";
-                path = "Rock_Beat#2.mp3";
-                B1.setActivated(true);
-            }
-        });
-        RB14.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                child = "Rock Beat 3.mp3";
-                path = "Rock_Beat#3.mp3";
-                B1.setActivated(true);
-            }
-        });
-        RB15.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                child = "rock beat 4.mp3";
-                path = "Rock_Beat#4.mp3";
-                B1.setActivated(true);
-            }
-        });
-        RB16.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                child = "Rock beat 5.mp3";
-                path = "Rock_Beat#5.mp3";
-                B1.setActivated(true);
-            }
-        });
-        RB5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                child = "R&B Beat_1.mp3";
-                path = "R&B_Beat#1.mp3";
-                B1.setActivated(true);
-
-            }
-        });
-        RB6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                child = "R&B Beat 2.mp3";
-                path = "R&B_Beat#2.mp3";
-                B1.setActivated(true);
-
-            }
-        });
-        RB7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                child = "R&B Beat 3.mp3";
-                path = "R&B_Beat#3.mp3";
-                B1.setActivated(true);
-
-            }
-        });
-        RB8.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                child = "R&B beat 4.mp3";
-                path = "R&B_Beat#4.mp3";
-                B1.setActivated(true);
-
-            }
-        });
-        RB9.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                child = "Country beat 1.mp3";
-                path = "Country_Beat#1.mp3";
-                B1.setActivated(true);
-
-            }
-        });
-        RB10.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                child = "Rap Beat 4.mp3";
-                path = "HipHop_Beat#4.mp3";
-                B1.setActivated(true);
-            }
-        });
-        RB11.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                child = "Rap Beat 5.mp3";
-                path = "HipHop_Beat#5.mp3";
-                B1.setActivated(true);
-            }
-        });
-        RB12.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                child = "R&B beat 5.mp3";
-                path = "R&B_Beat#5.mp3";
-                B1.setActivated(true);
-            }
-        });
-        RB17.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                child = "country beat 2.mp3";
-                path = "Country_Beat#2.mp3";
-                B1.setActivated(true);
-            }
-        });
-        RB18.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                child = "country beat 3.mp3";
-                path = "Country_Beat#3.mp3";
-                B1.setActivated(true);
-            }
-        });
-        RB19.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                child = "country-beat-4.mp3";
-                path = "Country_Beat#4.mp3";
-                B1.setActivated(true);
-            }
-        });
-        RB20.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                child = "country beat 5.mp3";
-                path = "Country_Beat#5.mp3";
-                B1.setActivated(true);
-            }
-        });
-        RB21.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                child = "Rap Beat 6.mp3";
-                path = "HipHop_Beat#6.mp3";
-                B1.setActivated(true);
-            }
-        });
-        RB22.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                child = "Rap Beat 7.mp3";
-                path = "HipHop_Beat#7.mp3";
-                B1.setActivated(true);
-            }
-        });
-        RB23.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                child = "R&B Beat 6.mp3";
-                path = "R&B_Beat#6.mp3";
-                B1.setActivated(true);
-            }
-        });
         B1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (B1.isActivated()) {
+                    getBeatName();
                     Download();
-                }
-                else
-                {
-                    Toast.makeText(getApplicationContext(),"Select a beat first",Toast.LENGTH_SHORT).show();
-                }
             }
         });
     }
@@ -314,17 +120,9 @@ public class DownloadedBeats extends AppCompatActivity {
                         {
                             Toast.makeText(getApplicationContext(), "Error Occurred", Toast.LENGTH_SHORT).show();
                         }
-                        radioGroup.clearCheck();
-                        B1.setActivated(false);
                     }
                 });
-                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        radioGroup.clearCheck();
-                        B1.setActivated(false);
-                    }
-                });
+                builder.setNegativeButton("Cancel",null);
                 builder.show();
             }
         else if(!dir.exists())
@@ -340,8 +138,6 @@ public class DownloadedBeats extends AppCompatActivity {
                     public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
                         loadingHelper.dismissDialog();
                         Toast.makeText(getApplicationContext(),"This Beat Has Successfully Downloaded",Toast.LENGTH_SHORT).show();
-                        radioGroup.clearCheck();
-                        B1.setActivated(false);
                     }
                 }).addOnFailureListener(new OnFailureListener() {
                     @Override
@@ -351,6 +147,126 @@ public class DownloadedBeats extends AppCompatActivity {
                     }
                 });
             }}}
+
+    public void getBeatName()
+    {
+        String str = (String) BeatSpinner.getSelectedItem();
+        if(str.equals(" Call Me Now"))
+        {
+            child = "Rap Beat 1.mp3";
+            path = "HipHop_Beat#1.mp3";
+        }
+        if(str.equals(" Blame Me"))
+        {
+            child = "Rap Beat 2.mp3";
+            path = "HipHop_Beat#2.mp3";
+        }
+        if(str.equals(" Uppercut"))
+        {
+            child = "Rap_Beat_3.mp3";
+            path = "HipHop_Beat#3.mp3";
+        }
+        if(str.equals(" Only At Night"))
+        {
+            child = "Rap Beat 4.mp3";
+            path = "HipHop_Beat#4.mp3";
+        }
+        if(str.equals(" Too Hot For You"))
+        {
+            child = "Rap Beat 5.mp3";
+            path = "HipHop_Beat#5.mp3";
+        }
+        if(str.equals(" Just Go"))
+        {
+            child = "Rap Beat 6.mp3";
+            path = "HipHop_Beat#6.mp3";
+        }
+        if(str.equals(" Let Me In"))
+        {
+            child = "Rap Beat 7.mp3";
+            path = "HipHop_Beat#7.mp3";
+        }
+        if(str.equals(" Where Did You Go"))
+        {
+            child = "Rock Beat 1.mp3";
+            path = "Rock_Beat#1.mp3";
+        }
+        if(str.equals(" Just By Myself"))
+        {
+            child = "Rock Beat 2.mp3";
+            path = "Rock_Beat#2.mp3";
+        }
+        if(str.equals(" Champion Of The Fight"))
+        {
+            child = "Rock Beat 3.mp3";
+            path = "Rock_Beat#3.mp3";
+        }
+        if(str.equals(" Unbeatable"))
+        {
+            child = "rock beat 4.mp3";
+            path = "Rock_Beat#4.mp3";
+        }
+        if(str.equals(" Dad And I"))
+        {
+            child = "Rock beat 5.mp3";
+            path = "Rock_Beat#5.mp3";
+        }
+        if(str.equals(" Late Night"))
+        {
+            child = "R&B Beat_1.mp3";
+            path = "R&B_Beat#1.mp3";
+        }
+        if(str.equals(" Scary Night"))
+        {
+            child = "R&B Beat 2.mp3";
+            path = "R&B_Beat#2.mp3";
+        }
+        if(str.equals(" On My Grind"))
+        {
+            child = "R&B Beat 3.mp3";
+            path = "R&B_Beat#3.mp3";
+        }
+        if(str.equals(" On My Mind"))
+        {
+            child = "R&B beat 4.mp3";
+            path = "R&B_Beat#4.mp3";
+        }
+        if(str.equals(" Out Like A Light"))
+        {
+            child = "R&B beat 5.mp3";
+            path = "R&B_Beat#5.mp3";
+        }
+        if(str.equals(" No Sleep For The Damned"))
+        {
+            child = "R&B Beat 6.mp3";
+            path = "R&B_Beat#6.mp3";
+        }
+        if(str.equals(" She's With Someone Else"))
+        {
+            child = "Country beat 1.mp3";
+            path = "Country_Beat#1.mp3";
+        }
+        if(str.equals(" Party Time"))
+        {
+            child = "country beat 2.mp3";
+            path = "Country_Beat#2.mp3";
+        }
+        if(str.equals(" Her Loss"))
+        {
+            child = "country beat 3.mp3";
+            path = "Country_Beat#3.mp3";
+        }
+        if(str.equals(" This Is My Town"))
+        {
+            child = "country-beat-4.mp3";
+            path = "Country_Beat#4.mp3";
+        }
+        if(str.equals(" What I Love The Most"))
+        {
+            child = "country beat 5.mp3";
+            path = "Country_Beat#5.mp3";
+        }
+    }
     public boolean isConnected()
     {
         boolean result;
